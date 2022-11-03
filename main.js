7979f89a61af771d36d48ebe5e9dde32eb3d21ef
soph-th <109481663+soph-th@users.noreply.github.com>
2022-11-03 14:52:53 +0000

    working

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 /* 
-👉 Write your kata here!
+
+👨‍🏫💥🥊👧 Playground Fight 👶💥🥊👩‍🏫
+
+Inspired by a conversation I once overheard between a couple on a date.
+
+'Oh, you teach Year 3? How many Year 3s do you reckon you could take?'
+
+1 teacher can win a fight against up to 8 year three kids.
+
+Each scenario will take in the number of teachers and the number of kids.
+
+Your function will need to return the winners (either kids or teachers).
+
+If there is no winner, return 'FIGHT FIGHT FIGHT'
+
+If either the kids or teachers input is equal to 0, return "Nominate fighter"
+
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+export function fightKid(teachers, kids) {
+}
--- main.test.js
+++ main.test.js
@@ -1,1 +1,29 @@
+import { test, expect } from "@jest/globals"; 
+
+import { fightKid } from './main.js';
+
 //👉 Write your tests below here:
+
+test('teachers should win', () => {
+    let actual = fightKid(20, 3);
+    let expected = 'teachers';
+    expect(actual.toLowerCase()).toBe(expected);
+})
+
+test('kids should win', () => {
+    let actual = fightKid(1, 14);
+    let expected = 'kids';
+    expect(actual.toLowerCase()).toBe(expected);
+})
+
+test('zero input, should be nominate fighter', () => {
+    let actual = fightKid(0, 3);
+    let expected = 'nominate fighter';
+    expect(actual.toLowerCase()).toBe(expected);
+})
+
+test('draw, so fight fight fight', () => {
+    let actual = fightKid(1, 8);
+    let expected = 'fight fight fight';
+    expect(actual.toLowerCase()).toBe(expected);
+})
